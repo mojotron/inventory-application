059fc9cc5cd42feee5c142c327c620e73659bd98
mojo <igormalj@gmail.com>
2024-09-21 18:33:18 +0200

    replace mongodb with postgresgl

--- db/connect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = (url) => {
-  // returns promise
-  return mongoose.connect(url);
-};
-
-module.exports = connectDB;
---
+++ db/queries.js
@@ -0,0 +1,3 @@
+import pool from './pool.js';
+
+export {};
